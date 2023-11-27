fba57957e8608ad0231d0cdd17ad35ac1a643cff
Thierry Vicente <119328995+StalkerVince@users.noreply.github.com>
2023-11-27 10:34:33 +0100

    first push

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my favourite movie list.");
+});
+
+app.get("/api/movies", (req, res) => {
+  res.json(movies);
+});
+
+app.get("/api/movies/:id", (req, res) => {
+  const wantedId = parseInt(req.params.id, 10);
+  const movieById = movies.find((e) => e.id === wantedId);
+
+  if (movieById != null) {
+    res.json(movieById);
+  } else {
+    res.sendStatus(404).send("Not Found");
+  }
+});
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
+
+/* Lance node app.js dans le terminal */
+/* http://localhost:5000/ */
+/* http://localhost:5000/api/movies */
+/* http://localhost:5000/api/movies/:id */
